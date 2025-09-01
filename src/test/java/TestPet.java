8a669fe05d005e31c7f2738ee991b8e207eead5a
orindi10 <orindi10@gmail.com>
2025-09-01 18:54:19 -0300

    Troca da entidade User por Pet

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,9 +1,9 @@
 import org.junit.jupiter.api.Test;
 
-public class TestUser {
+public class TestPet {
     
     static String ct = "application/json"; //content-type
-    static String uriUser = "https://petstore.swagger.io/v2/user";
+    static String uriUser = "https://petstore.swagger.io/v2/pet";
 
 
     public static String lerArquivoJson(String caminhoJson){
