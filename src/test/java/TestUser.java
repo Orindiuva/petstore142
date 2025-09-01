a198e9489a3d088f59f26b6d058619010495ca6b
orindi10 <orindi10@gmail.com>
2025-09-01 18:52:26 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class TestUser {
+    
+    static String ct = "application/json"; //content-type
+    static String uriUser = "https://petstore.swagger.io/v2/user";
+
+
+    public static String lerArquivoJson(String caminhoJson){
+
+        return null; 
+    }
+
+    @Test
+    public void testPostPet(){
+        //carregar os dados do arquivo JSON do pet
+        String jsonBody = "";
+
+    }
+
+
+}
