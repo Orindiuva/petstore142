61b0514fd5ccb07f0e1acdb48bb2c41f3ed8f3e4
orindi10 <orindi10@gmail.com>
2025-09-02 10:24:54 -0300

    Alterações API - Pet

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class TestPet {
     
@@ -6,17 +11,29 @@ public class TestPet {
     static String uriUser = "https://petstore.swagger.io/v2/pet";
 
 
-    public static String lerArquivoJson(String caminhoJson){
-
-        return null; 
+    public static String lerArquivoJson(String arquivoJson) throws IOException{                 
+         return new String(Files.readAllBytes(Paths.get(arquivoJson)));
     }
 
     @Test
-    public void testPostPet(){
+    public void testPostPet() throws IOException{
         //carregar os dados do arquivo JSON do pet
-        String jsonBody = "";
-
+        String jsonBody = lerArquivoJson("src/test/resources/json/pet1.json");
+        boolean tst = jsonBody == null; 
+        System.out.println("boolean: " + tst);
+        int petId = 173998;
+        given().
+            contentType(ct).
+            log().all().                                         //mostra tudo na ida
+            body(jsonBody)                                       //envia o corpo da requisicao
+        .when()
+            .post(uriUser)                                      //Chamamos o endpoint com o corpo da requisicao
+        .then()
+            .log().all()                                        //mostra tudo na volta
+            .statusCode(200)
+            .body("name", is("snoopy"))
+            .body("id", is(petId))
+            .body("category.name", is("cachorro"))
+            .body("tags[0].name", is("vacinado"));        
     }
-
-
 }
